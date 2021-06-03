681a1a7794961167f2865cc5115f9b97bf271254
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-06-03 17:23:31 -0400

    solution reverse 1

--- exercises/1_reversestring/index.js
+++ exercises/1_reversestring/index.js
@@ -2,10 +2,17 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {}
 
 module.exports = reverse;
+
+// Solution 1
+/* function reverse(str) {
+    const arrayNew = str.split('');
+    arrayNew.reverse();
+    return arr.arrayNew('');
+  } */
