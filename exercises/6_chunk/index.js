b9b5a0cea896ac722a09f6e53736c6b3ba01734b
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-06-04 07:47:25 -0400

    Chunk with for of

---
+++ exercises/6_chunk/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  const bulk = [];
+
+  for (let element of array) {
+    const last = bulk[bulk.length - 1];
+
+    if (!last || last.length === size) {
+      bulk.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return bulk;
+}
+
+module.exports = chunk;
+
+// https://stackoverflow.com/questions/29285897/what-is-the-difference-between-for-in-and-for-of-statements
+
+// Solution 1
+// Storage
+/*   const bulk = [];
+
+  for (let element of array) {
+    const last = bulk[bulk.length - 1];
+
+    if (!last || last.length === size) {
+      bulk.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return bulk; */
